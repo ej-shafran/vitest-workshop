50b4fe35f445fa9bd7d6707f651e747d70672181
ej-shafran <ejshafran@hilma.tech>
2024-03-05 11:57:17 +0200

    Third property

--- src/add.spec.ts
+++ src/add.spec.ts
@@ -17,8 +17,15 @@ describe("add", () => {
 
   test("adding 1 twice is the same as adding 2", () => {
     for (let i = 0; i < 100; i++) {
-      const a = randomInt()
+      const a = randomInt();
       expect(add(add(a, 1), 1)).toBe(add(a, 2));
+    }
+  });
+
+  test("adding 0 is the same as doing nothing", () => {
+    for (let i = 0; i < 100; i++) {
+      const a = randomInt();
+      expect(add(a, 0)).toBe(a);
     }
   });
 });
