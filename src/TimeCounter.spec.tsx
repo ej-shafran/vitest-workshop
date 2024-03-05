9ce48d31d02ec1a0a78c2c70b8998147120976b1
ej-shafran <ejshafran@hilma.tech>
2024-03-05 13:41:12 +0200

    Proper tests

--- src/TimeCounter.spec.tsx
+++ src/TimeCounter.spec.tsx
@@ -1,17 +1,57 @@
-import React from "react";
-import { describe, expect, it } from "vitest";
-import { render } from "@testing-library/react";
-import TimeCounter from "./TimeCounter";
+import { cleanup, render } from "@testing-library/react";
 import dayjs from "dayjs";
+import fc from "fast-check";
+import { describe, it, expect } from "vitest";
+
+import TimeCounter from "./TimeCounter";
+
+const max59Arb = fc.nat({ max: 59 });
+const underHourDurationArb = fc
+  .record({ seconds: max59Arb, minutes: max59Arb })
+  .map(dayjs.duration);
+const overHourDurationArb = fc
+  .record({
+    seconds: max59Arb,
+    minutes: max59Arb,
+    hours: fc.integer({ min: 1 }),
+  })
+  .map(dayjs.duration);
 
 describe("TimeCounter", () => {
-  it("should show 1:00 after a minute", async () => {
-    const screen = render(
-      <TimeCounter
-        emergencyStart={dayjs().subtract(dayjs.duration({ minutes: 1 }))}
-      />,
-    );
+  it("should show mm:ss if under 1hr", () => {
+    const property = fc.property(underHourDurationArb, (duration) => {
+      const emergencyStart = dayjs().subtract(duration);
 
-    expect(screen.getByTestId("time-counter")).toHaveTextContent("1:00")
+      const screen = render(<TimeCounter emergencyStart={emergencyStart} />);
+
+      const timeCounter = screen.getByTestId("time-counter");
+      expect(timeCounter).toBeInTheDocument();
+      expect(timeCounter.textContent).toBeTruthy();
+
+      const [minutes, seconds] = timeCounter.textContent!.split(":");
+      expect(parseInt(minutes)).toBe(duration.minutes());
+      expect(parseInt(seconds)).toBe(duration.seconds());
+    });
+
+    fc.assert(property.beforeEach(cleanup));
+  });
+
+  it("should show HH:mm:ss if over 1hr", () => {
+    const property = fc.property(overHourDurationArb, (duration) => {
+      const emergencyStart = dayjs().subtract(duration);
+
+      const screen = render(<TimeCounter emergencyStart={emergencyStart} />);
+
+      const timeCounter = screen.getByTestId("time-counter");
+      expect(timeCounter).toBeInTheDocument();
+      expect(timeCounter.textContent).toBeTruthy();
+
+      const [hours, minutes, seconds] = timeCounter.textContent!.split(":");
+      expect(parseInt(hours)).toBe(duration.hours());
+      expect(parseInt(minutes)).toBe(duration.minutes());
+      expect(parseInt(seconds)).toBe(duration.seconds());
+    });
+
+    fc.assert(property.beforeEach(cleanup));
   });
 });
