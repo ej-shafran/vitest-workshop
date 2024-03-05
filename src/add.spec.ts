121c22c2839d7c8d5f7e6e00398f34c67401eec5
ej-shafran <ejshafran@hilma.tech>
2024-03-05 11:50:17 +0200

    Random inputs

--- src/add.spec.ts
+++ src/add.spec.ts
@@ -1,17 +1,19 @@
-import { describe, expect, test, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import { add } from "./add";
 
-function check(a: number, b: number, result: number) {
-  test(`${a} + ${b} = ${result}`, () => {
-    expect(add(a, b)).toBe(result);
-  });
+function randomInt() {
+  return Math.floor(Math.random() * 10_000);
 }
 
 describe("add", () => {
-  check(2, 2, 4);
-  check(1, 3, 4);
-  check(5, 3, 8);
-  check(27, -3, 24);
-  check(3, 2, 5);
+  it("should work for any integers", () => {
+    for (let i = 0; i < 100; i++) {
+      const a = randomInt();
+      const b = randomInt();
+      const expected = a + b;
+      const result = add(a, b);
+      expect(result).toBe(expected);
+    }
+  });
 });
