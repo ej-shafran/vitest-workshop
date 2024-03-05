0dec71c51c02fda9da4a5b9ac2abba56d64d2b17
ej-shafran <ejshafran@hilma.tech>
2024-03-05 12:04:43 +0200

    Using a Library

--- src/add.spec.ts
+++ src/add.spec.ts
@@ -1,31 +1,30 @@
 import { describe, expect, it, test } from "vitest";
+import fc from "fast-check";
 
 import { add } from "./add";
 
-function randomInt() {
-  return Math.floor(Math.random() * 10_000);
-}
-
 describe("add", () => {
   it("should not depend on param order", () => {
-    for (let i = 0; i < 100; i++) {
-      const a = randomInt();
-      const b = randomInt();
+    const property = fc.property(fc.integer(), fc.integer(), (a, b) => {
       expect(add(a, b)).toBe(add(b, a));
-    }
+    });
+
+    fc.assert(property);
   });
 
   test("adding 1 twice is the same as adding 2", () => {
-    for (let i = 0; i < 100; i++) {
-      const a = randomInt();
+    const property = fc.property(fc.integer(), (a) => {
       expect(add(add(a, 1), 1)).toBe(add(a, 2));
-    }
+    });
+
+    fc.assert(property);
   });
 
   test("adding 0 is the same as doing nothing", () => {
-    for (let i = 0; i < 100; i++) {
-      const a = randomInt();
+    const property = fc.property(fc.integer(), (a) => {
       expect(add(a, 0)).toBe(a);
-    }
+    });
+
+    fc.assert(property);
   });
 });
