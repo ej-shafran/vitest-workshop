b22b93ef0bdd4f2cdc40fc22e348703e55ace21f
ej-shafran <ejshafran@hilma.tech>
2024-04-09 15:57:02 +0300

    fast-check fix

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,10 @@
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
+import fc from "fast-check";
 import { beforeEach, expect } from "vitest";
 
 expect.extend(matchers);
 beforeEach(cleanup);
+fc.configureGlobal({
+    beforeEach: cleanup,
+});
